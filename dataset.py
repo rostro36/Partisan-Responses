ae17d246cd3092f0834d29fea77c63acc8655f6d
Mian Zhong <mian.zhong@berkeley.edu>
2020-12-08 15:17:27 +0100

    integrate dataset

--- dataset.py
+++ dataset.py
@@ -1,8 +1,13 @@
 import pandas as pd 
 import os 
+from tqdm import tqdm 
+import re
+from bs4 import BeautifulSoup
+import requests
+import pickle
+import csv 
 import nltk 
 from nltk.stem import PorterStemmer
-from tqdm import tqdm 
 from utils import *
 
 class Heinbound:
@@ -247,7 +252,8 @@ class Debate(PresidencyProject):
     """
     Presidential Campaign Debate
     """
-    self.url = "https://www.presidency.ucsb.edu/documents/presidential-documents-archive-guidebook/presidential-campaigns-debates-and-endorsements-0"
+    def __init__(self):
+        self.url = "https://www.presidency.ucsb.edu/documents/presidential-documents-archive-guidebook/presidential-campaigns-debates-and-endorsements-0"
     def collect_debate_guidebook(url, save_direc):
         statistics = {"presidential":0, "democrat":0, "republican":0}
         debate_id, year, category = 0, None, None
@@ -301,3 +307,65 @@ class Debate(PresidencyProject):
         elif "republican" in cat_text:
             category = "republican"
         return year, category
+
+
+#collect_debate_guidebook(url, presidency_proj_direc)
+guidebook = pd.read_csv(os.path.join(presidency_proj_direc,"presidency_project_debates_guidebook.csv"))
+dem = guidebook[guidebook['category'] == 'republican']
+# Extract debate script
+# Democrat
+
+for i in range(dem.shape[0]):
+    url = dem.iloc[i]['url']
+    debate_id = dem.iloc[i]['debate_id']
+    debate_html = requests.get(url)
+    soup = BeautifulSoup(debate_html.text, "html.parser")
+    script = soup.findAll('div', {'class': 'field-docs-content'})
+
+    if len(script) == 1:
+        speeches = script[0].find_all('p')
+        current_speaker = None
+        current_party = None #None if not candidate
+        date = soup.find("div", {"class":"field-docs-start-date-time"}).span['content']
+        p_id = 0
+        try:
+            start = speeches[p_id].contents[0].text
+        except AttributeError:
+            start = speeches[p_id].contents[0]
+        if start.upper() == "PARTICIPANTS:": #skip to next <p>
+            p_id += 1
+        start = speeches[p_id]
+        try:
+            if start.contents[0].text.lower().startswith("moderator"):
+                p = re.compile("([Mm][Oo][Dd][Ee][Rr][Aa][Tt][Oo][Rr][Ss]?:)|(\([\.\w\s-]+\))") # remove string moderator(s): and organization
+                moderators = [i.strip() for i in p.sub('', speeches[1].text.replace("and", "")).split(";")]
+                transtable = str.maketrans("áéíóúÁÉÍÓÚ", "aeiouAEIOU")
+                moderators_lastname = [name.split(",")[0].split(" ")[-1].translate(transtable).lower() for name in moderators]
+                p_id += 1
+            speech_id = 0
+            for speech in speeches[p_id:]:
+                if len(speech.contents) == 2:
+                    current_speaker = speech.contents[0].text.replace(":", "").lower()
+                    if current_speaker not in moderators_lastname and current_speaker != "q":
+                        current_party = "R" #todo
+                    else:
+                        current_party = "M"
+                    speech = speech.contents[1].strip()
+                else:
+                    speech = speech.text
+                record = [debate_id, speech_id, current_speaker, speech, current_party]
+                csvwriter.writerow(record)
+                speech_id += 1
+        except AttributeError:
+            continue
+    else:
+        print("More than 1 script found on the url:")
+        print(url)
+        print("="*20)
+# write to file
+f = open(os.path.join(presidency_proj_direc,"republican_candidates_debates_speeches.csv"), 'w', newline='')
+csvwriter = csv.writer(f, delimiter=",")
+csvwriter.writerow(['debate_id', 'speech_id', 'speaker', 'speech', 'party'])
+f.close()
+#unique_start = {'BRIT HUME, FOX NEWS:', 'PARTICIPANTS:', 'WOLF BLITZER:', 'Participants:', 'Moderators:', 'TOM BROKAW:'}
+#unique_start = {'PARTICIPANTS:', 'ANNOUNCER:', 'Moderators:', 'COKIE ROBERTS:', 'Participants:'}
